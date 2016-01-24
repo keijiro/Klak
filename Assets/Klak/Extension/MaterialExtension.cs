e47d020aa915d89313dbe78f23c3d342d1b0f8fd
Keijiro Takahashi <keijiro@ZX.local>
2016-01-24 23:19:38 +0900

    Cosmetc changes.

--- Assets/Klak/Extension/MaterialExtension.cs
+++ Assets/Klak/Extension/MaterialExtension.cs
@@ -23,124 +23,121 @@
 //
 using UnityEngine;
 
-namespace Klak
+namespace Klak.MaterialExtension
 {
-    namespace MaterialExtension
+    /// Extension methods (setters) for Material
+    static class MaterialSetterExtension
     {
-        /// Extension methods (setters) for Material
-        static class MaterialSetterExtension
+        static public Material Property(this Material m, string name, float x)
         {
-            static public Material Property(this Material m, string name, float x)
-            {
-                m.SetFloat(name, x);
-                return m;
-            }
-
-            static public Material Property(this Material m, string name, float x, float y)
-            {
-                m.SetVector(name, new Vector2(x, y));
-                return m;
-            }
-
-            static public Material Property(this Material m, string name, float x, float y, float z)
-            {
-                m.SetVector(name, new Vector3(x, y, z));
-                return m;
-            }
-
-            static public Material Property(this Material m, string name, float x, float y, float z, float w)
-            {
-                m.SetVector(name, new Vector4(x, y, z, w));
-                return m;
-            }
-
-            static public Material Property(this Material m, string name, Vector2 v)
-            {
-                m.SetVector(name, v);
-                return m;
-            }
-
-            static public Material Property(this Material m, string name, Vector3 v)
-            {
-                m.SetVector(name, v);
-                return m;
-            }
-
-            static public Material Property(this Material m, string name, Vector4 v)
-            {
-                m.SetVector(name, v);
-                return m;
-            }
-
-            static public Material Property(this Material m, string name, Color color)
-            {
-                m.SetColor(name, color);
-                return m;
-            }
-
-            static public Material Property(this Material m, string name, Texture texture)
-            {
-                m.SetTexture(name, texture);
-                return m;
-            }
+            m.SetFloat(name, x);
+            return m;
         }
 
-        /// Extension methods (setters) for MaterialProperty
-        static class MaterialPropertySetterExtension
+        static public Material Property(this Material m, string name, float x, float y)
         {
-            static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, float x)
-            {
-                m.SetFloat(name, x);
-                return m;
-            }
+            m.SetVector(name, new Vector2(x, y));
+            return m;
+        }
 
-            static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, float x, float y)
-            {
-                m.SetVector(name, new Vector2(x, y));
-                return m;
-            }
+        static public Material Property(this Material m, string name, float x, float y, float z)
+        {
+            m.SetVector(name, new Vector3(x, y, z));
+            return m;
+        }
 
-            static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, float x, float y, float z)
-            {
-                m.SetVector(name, new Vector3(x, y, z));
-                return m;
-            }
+        static public Material Property(this Material m, string name, float x, float y, float z, float w)
+        {
+            m.SetVector(name, new Vector4(x, y, z, w));
+            return m;
+        }
 
-            static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, float x, float y, float z, float w)
-            {
-                m.SetVector(name, new Vector4(x, y, z, w));
-                return m;
-            }
+        static public Material Property(this Material m, string name, Vector2 v)
+        {
+            m.SetVector(name, v);
+            return m;
+        }
 
-            static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Vector2 v)
-            {
-                m.SetVector(name, v);
-                return m;
-            }
+        static public Material Property(this Material m, string name, Vector3 v)
+        {
+            m.SetVector(name, v);
+            return m;
+        }
 
-            static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Vector3 v)
-            {
-                m.SetVector(name, v);
-                return m;
-            }
+        static public Material Property(this Material m, string name, Vector4 v)
+        {
+            m.SetVector(name, v);
+            return m;
+        }
 
-            static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Vector4 v)
-            {
-                m.SetVector(name, v);
-                return m;
-            }
+        static public Material Property(this Material m, string name, Color color)
+        {
+            m.SetColor(name, color);
+            return m;
+        }
 
-            static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Color color)
-            {
-                m.SetColor(name, color);
-                return m;
-            }
+        static public Material Property(this Material m, string name, Texture texture)
+        {
+            m.SetTexture(name, texture);
+            return m;
+        }
+    }
 
-            static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Texture texture)
-            {
-                m.SetTexture(name, texture);
-                return m;
-            }
+    /// Extension methods (setters) for MaterialProperty
+    static class MaterialPropertySetterExtension
+    {
+        static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, float x)
+        {
+            m.SetFloat(name, x);
+            return m;
+        }
+
+        static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, float x, float y)
+        {
+            m.SetVector(name, new Vector2(x, y));
+            return m;
+        }
+
+        static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, float x, float y, float z)
+        {
+            m.SetVector(name, new Vector3(x, y, z));
+            return m;
+        }
+
+        static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, float x, float y, float z, float w)
+        {
+            m.SetVector(name, new Vector4(x, y, z, w));
+            return m;
+        }
+
+        static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Vector2 v)
+        {
+            m.SetVector(name, v);
+            return m;
+        }
+
+        static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Vector3 v)
+        {
+            m.SetVector(name, v);
+            return m;
+        }
+
+        static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Vector4 v)
+        {
+            m.SetVector(name, v);
+            return m;
+        }
+
+        static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Color color)
+        {
+            m.SetColor(name, color);
+            return m;
+        }
+
+        static public MaterialPropertyBlock Property(this MaterialPropertyBlock m, string name, Texture texture)
+        {
+            m.SetTexture(name, texture);
+            return m;
         }
     }
 }
