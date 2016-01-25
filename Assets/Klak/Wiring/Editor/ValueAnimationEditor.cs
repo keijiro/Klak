d37c1079d76bffb14ec24e0b01088a95bf60934c
Keijiro Takahashi <keijiro@ZX.local>
2016-01-25 22:40:44 +0900

    Add ValueAnimation

---
+++ Assets/Klak/Wiring/Editor/ValueAnimationEditor.cs
@@ -0,0 +1,88 @@
+//
+// Klak - Utilities for creative coding with Unity
+//
+// Copyright (C) 2016 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+using UnityEngine;
+using UnityEditor;
+
+namespace Klak.Wiring
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(ValueAnimation))]
+    public class ValueAnimationEditor : Editor
+    {
+        SerializedProperty _animations;
+        SerializedProperty _animationIndex;
+        SerializedProperty _playOnStart;
+        SerializedProperty _speed;
+        SerializedProperty _valueEvent;
+
+        void OnEnable()
+        {
+            _animations = serializedObject.FindProperty("_animations");
+            _animationIndex = serializedObject.FindProperty("_animationIndex"); 
+            _playOnStart = serializedObject.FindProperty("_playOnStart");
+            _speed = serializedObject.FindProperty("_speed");
+            _valueEvent = serializedObject.FindProperty("_valueEvent");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            ShowAnimationList(_animations);
+
+            EditorGUILayout.PropertyField(_animationIndex);
+            EditorGUILayout.PropertyField(_playOnStart);
+            EditorGUILayout.PropertyField(_speed);
+            EditorGUILayout.PropertyField(_valueEvent);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        void ShowAnimationList(SerializedProperty animations)
+        {
+            var count = animations.arraySize;
+
+            // FIXME: should be replaced with DelayedIntField in 5.3
+            count = EditorGUILayout.IntField("Animation Count", count);
+            count = Mathf.Max(count, 1);
+
+            // enlarge/shrink the list when the size is changed
+            while (count > animations.arraySize)
+                animations.InsertArrayElementAtIndex(animations.arraySize - 1);
+            while (count < animations.arraySize)
+                animations.DeleteArrayElementAtIndex(animations.arraySize - 1);
+
+            EditorGUI.indentLevel++;
+
+            for (var i = 0; i < animations.arraySize; i++)
+            {
+                var data = animations.GetArrayElementAtIndex(i);
+                var label_i = new GUIContent("Animation " + i);
+                EditorGUILayout.PropertyField(data, label_i);
+            }
+
+            EditorGUI.indentLevel--;
+        }
+    }
+}
---
+++ Assets/Klak/Wiring/ValueAnimation.cs
@@ -0,0 +1,116 @@
+//
+// Klak - Utilities for creative coding with Unity
+//
+// Copyright (C) 2016 Keijiro Takahashi
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Klak.Wiring
+{
+    [AddComponentMenu("Klak/Wiring/Value Animation")]
+    public class ValueAnimation : MonoBehaviour
+    {
+        #region Nested Public Classes
+
+        [System.Serializable]
+        public class ValueEvent : UnityEvent<float> {}
+
+        #endregion
+
+        #region Editable Properties
+
+        [SerializeField]
+        AnimationCurve[] _animations = new AnimationCurve[1] {
+            AnimationCurve.Linear(0, 0, 1, 1)
+        };
+
+        [SerializeField]
+        int _animationIndex = 0;
+
+        [SerializeField]
+        bool _playOnStart = false;
+
+        [SerializeField]
+        float _speed = 1.0f;
+
+        [SerializeField]
+        ValueEvent _valueEvent;
+
+        #endregion
+
+        #region Public Properties And Methods
+
+        public int animationIndex {
+            get { return _animationIndex; }
+            set {
+                _animationIndex = Mathf.Clamp(value, 0, _animations.Length - 1);
+            }
+        }
+
+        public float speed {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public float time { get; set; }
+
+        public bool isPlaying { get; set; }
+
+        public void Play(int index)
+        {
+            animationIndex = index;
+            time = 0;
+            isPlaying = true;
+        }
+
+        public void Pause()
+        {
+            isPlaying = false;
+        }
+
+        public void Resume()
+        {
+            isPlaying = true;
+        }
+
+        #endregion
+
+        #region MonoBehaviour Functions
+
+        void Start()
+        {
+            isPlaying = _playOnStart;
+        }
+
+        void Update()
+        {
+            if (isPlaying)
+            {
+                time += Time.deltaTime * _speed;
+                var v = _animations[_animationIndex].Evaluate(time);
+                _valueEvent.Invoke(v);
+            }
+        }
+
+        #endregion
+    }
+}
